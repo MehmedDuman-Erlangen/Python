3ea6c94618a6922a732173ba1bc2c26f7ca699c5
MehmedDuman-Erlangen <114074965+MehmedDuman-Erlangen@users.noreply.github.com>
2022-09-21 14:49:53 +0200

    Update python_practise.py
    My version of Python best practice

--- Exam_Exercise/python_practise.py
+++ Exam_Exercise/python_practise.py
@@ -1,45 +1,41 @@
-'''import fractions
-print(fractions.Fraction(1.5))'''
-
-import fractions 
 import decimal
-from fractions import Fraction as F
 from decimal import Decimal as D
-'''
-print(F(D('1.3')))
-print(F(D('1.5')))
-print(D('1.3')) 
-#print(D(1.3)) 
-print(F(1,3) + F(1,3))
-print(F(1.3) + F(1.3))
-print(F('1.3') + F('1.3'))
-print(F("2.6"))
+# Beipsiele wann man decimal benützen sollte wären
+# 1. Banken/Finanzen
+# 2. Medizin
+# 3. Teilchen Pyhisk
+import fractions 
+from  fractions import Fraction as F
+print("First way of printing fractions:",F('1.5'))
+print("second way of printing fractions:",fractions.Fraction(1.5))
+#Side Notes: You can import Modules bewteen line like: import fractions <next line> from fractions import Fractions as F
+print(F(D(1.5))) #Wieso mal Strings und wieso mal keine Strings
+print(F(1,3)+F(1,3))
+print(F(4,3))
+print(F(1.5))
+print("This gives out a dynamic integer number, wich python will convert to a decimal number",D(1.3))
+print("This will give a static number, since we put the integer number in a string wich is an absolute number",D("1.3"))
+print("Here will be 1.5 convertet to an asbolute static integer Number",F(D(1.5)))
 
-
-i = 42
-print(type(i))
-a = '''
-
-X = [3, 0, "Lid"]
-print(X[2])
-y = [0, 2, (3, 4), [1,3], "Lid" ]
-print(y[2])
-S = (2, 0, 1, (3, 3))
-print(S[3])
-text = "01234567"
-text = "Hello"
-print(text[0:4])
-
-upperTest = ("hello world").upper() #Upper writes the string all in big capitals
-print(upperTest)
-
-import time 
-n = 1000
-start_time = time.time()
-liste = []
-for i in range(n):
-    liste = liste + [i * 2]
-
-print(time.time() - start_time)
-print(time.time())
+a = 42
+b = 42,5
+c = "42"
+d = 42.7
+e = "Is this a string?"
+dec = D(d)
+print("Type a entspricht:",type(a))
+print("Type b is:",type(b))
+print("Type C is:",type(c))
+print("Type d is",type(d))
+print("Is Type e a String?:",isinstance(e,str))
+print(int(dec))
+#implizyte type converting is similar to automatic type converting performed of python 
+#explicit type converting is done mannually by the user to convert a type to a specific type he requires throw defined type functions
+randomDynamicList = [3,99,"sample String"] #You can edit, index, change, delete or add elements to a dictionary List
+randomStaticList = (3,99,"Sample String") #You can NOT edit, change, delete or add elements to a tuble list
+print(randomStaticList)
+randomDynamicList.extend(["This can be a list or just a variable"]) #Extends just an element wich increases the range of the current list
+#If you extend a list with a String, you have to put square brackets [] around the string, or it will just split the string in to seperate charackters
+randomDynamicList.append([1,2,3]) #append adds a seperate list to the current existing list
+print(randomDynamicList)
 
